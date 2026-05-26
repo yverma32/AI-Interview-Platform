a945dd1d8ff6f8607bf7f3fba13c96ac2da36b1c
Yash-Verma <yverma32@gmail.com>
2026-05-26 16:45:32 +0530

    feat(db): add IsFoundingMember migration

---
+++ InterviewPlatform.API/Migrations/20260526111521_AddFoundingMember.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewPlatform.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFoundingMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFoundingMember",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFoundingMember",
+                table: "Users");
+        }
+    }
+}
